2bee486ca3af31dc97cc8496d8e2a0f9c93f76b6
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-20 11:17:13 -0400

    song attributes/ relationships

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 
 class Instrument(models.Model):
+    # attributes
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     name = models.CharField(max_length=200)
@@ -12,23 +13,27 @@ class Instrument(models.Model):
     tonal_range = models.CharField(max_length=200)
     picture = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     year = models.CharField(max_length=4)
+    # relationships
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    songs = models.ManyToManyField(Song)
-    elements = models.ManyToManyField(Element)
+    songs = models.ManyToManyField(Song, on_delete=models.DO_NOTHING)
+    elements = models.ManyToManyField(Element, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
 
 
 class Song(models.Model):
-    album = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
-    year = models.CharField(max_length=200)
+    album = models.CharField(max_length=200, blank=True)
+    year = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    genre = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200, blank=True)
     duration = models.IntegerField(blank=True)
-    name = models.CharField(max_length=200)
     explicit = models.BooleanField(default=False)
-    key = models.IntegerField(blank=true)
-    mode = models.IntegerField(blank=true)
+    key = models.IntegerField(blank=True)
+    mode = models.IntegerField(blank=True)
     time_signature = models.IntegerField(blank=true)
     tempo = models.FloatField(blank=True)
     acousticness = models.FloatField(blank=True)
@@ -40,11 +45,10 @@ class Song(models.Model):
     speechiness = models.FloatField(blank=True)
     valence = models.FloatField(blank=True)
     original = models.BooleanField(default=False)
-    spotify_url = models.CharField(max_length=200)
-    spotify_id = models.CharField(max_length=200)
+    spotify_url = models.CharField(max_length=200, blank=True)
+    spotify_id = models.CharField(max_length=200, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-
-# relationships:
-# belongs to user
-# has many instruments(through elements?)
-# has many elements
+    def __str__(self):
+        return self.name
