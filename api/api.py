32582fcb7878da600d3a633daa57541be7bbadd3
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-20 17:21:22 -0400

    model view sets

---
+++ api/api.py
@@ -0,0 +1,30 @@
+from .models import Instrument, Song, Element, File
+from rest_framework import viewsets
+from .serializers import InstrumentSerializer, SongSerializer, ElementSerialzer, FileSerializer
+
+# Project Info viewset
+# allows us to create a CRUD api without specifying methods for functionality
+
+
+class InstrumentViewSet(viewsets.ModelViewSet):
+
+    queryset = Instrument.objects.all()
+    serializer_class = InstrumentSerializer
+
+
+class SongViewSet(viewsets.ModelViewSet):
+
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+class ElementViewSet(viewsets.ModelViewSet):
+
+    queryset = Element.objects.all()
+    serializer_class = ElementSerializer
+
+
+class FileViewSet(viewsets.ModelViewSet):
+
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
