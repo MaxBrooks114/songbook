42c9a229cb014712e5a6b5e304e30f8478187b22
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-20 14:11:55 -0400

    register admin models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Instrument
+from .models import Song
+from .models import Element
+from .models import File
+
 
 # Register your models here.
+admin.site.register(Instrument)
+admin.site.register(Song)
+admin.site.register(Element)
+admin.site.register(File)
