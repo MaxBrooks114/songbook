43caaec489f8c042cf59464385788ef43e164764
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-09-22 10:10:35 -0400

    nested serializers for song and elements

--- api/serializers.py
+++ api/serializers.py
@@ -50,18 +50,53 @@ class InstrumentSerializer(serializers.ModelSerializer):
                   'tonal_range', 'year']
 
 
+def get_primary_key_related_model(model_class, **kwargs):
+    """
+    Nested serializers are a mess. https://stackoverflow.com/a/28016439/2689986
+    This lets us accept ids when saving / updating instead of nested objects.
+    Representation would be into an object (depending on model_class).
+    """
+    class PrimaryKeyNestedMixin(model_class):
+
+        def to_internal_value(self, data):
+            try:
+                return model_class.Meta.model.objects.get(pk=data)
+            except model_class.Meta.model.DoesNotExist:
+                self.fail('does_not_exist', pk_value=data)
+            except (TypeError, ValueError):
+                self.fail('incorrect_type', data_type=type(data).__name__)
+
+        def to_representation(self, data):
+            return model_class.to_representation(self, data)
+
+    return PrimaryKeyNestedMixin(**kwargs)
+
+
 class SongSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'year', 'image', 'genre', 'duration', 'explicit', 'key', 'mode', 'lyrics', 'time_signature', 'tempo',
-                  'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'valence', 'original', 'spotify_url', 'spotify_id']
+                  'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'valence', 'original', 'spotify_url', 'spotify_id', 'elements']
 
 
 class ElementSerializer(serializers.ModelSerializer):
+
+    song = get_primary_key_related_model(SongSerializer)
+
     class Meta:
         model = Element
-        fields = ['name', 'start', 'duration', 'loudness', 'tempo',
-                  'key', 'mode', 'lyrics', 'learned', 'time_signature']
+        fields = ["id", "name",
+                  "start",
+                  "duration",
+                  "loudness",
+                  "tempo",
+                  "key",
+                  "mode",
+                  "lyrics",
+                  "learned",
+                  "time_signature",
+                  "song"]
 
 
 class FileSerializer(serializers.ModelSerializer):
