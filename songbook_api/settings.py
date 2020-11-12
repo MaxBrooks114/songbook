a5416e89c6894e6811c8df4aa6a7cb4efc3fc0da
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-11-12 11:36:47 -0500

    add token expiration time

--- songbook_api/settings.py
+++ songbook_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from datetime import timedelta
+from rest_framework.settings import api_settings
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -145,3 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_KNOX = {
+    'TOKEN_TTL': None,
+}
